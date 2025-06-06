eaaa8267f50e97ae06c58c80322de08f7a474887
Artem Kabritskii <artemkabr7@gmail.com>
2025-06-06 10:47:47 +0300

    fix: добавлен return type в mock_json для mypy

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,10 @@
 from pathlib import Path
+from unittest.mock import Mock, patch
 
 import pandas as pd
 import pytest
 
+from src import utils
 from src.utils import load_transactions_from_excel
 
 
@@ -22,3 +24,28 @@ def test_load_transactions_from_excel(tmp_path: Path) -> None:
 def test_load_transactions_file_not_found() -> None:
     with pytest.raises(RuntimeError):
         load_transactions_from_excel("non_existing_file.xlsx")
+
+
+@patch("requests.get")
+def test_get_currency_rates(mock_get: Mock) -> None:
+    mock_response = Mock()
+    mock_response.json.return_value = {"rates": {"USD": 90, "EUR": 100}}
+    mock_response.status_code = 200
+    mock_get.return_value = mock_response
+
+    result = utils.get_currency_rates(["USD", "EUR"])
+    assert result == {"USD": 90.0, "EUR": 95.5}
+
+
+@patch("requests.get")
+def test_get_stock_prices(mock_get: Mock) -> None:
+    def mock_json() -> dict[str, int]:
+        return {"c": 150}
+
+    mock_response = Mock()
+    mock_response.json.side_effect = mock_json
+    mock_response.status_code = 200
+    mock_get.return_value = mock_response
+
+    result = utils.get_stock_prices(["AAPL", "AMZN", "GOOGL"])
+    assert result == {"AAPL": 100.0, "AMZN": 150.0, "GOOGL": 200.0}
